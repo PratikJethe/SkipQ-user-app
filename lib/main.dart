7b8bb2dd88ee2c3d8bc55286acce1df4c659e64b
pratik jethe <pratikjethe93@gmail.com>
2022-02-02 21:57:43 +0530

    added latest version check

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:skipq/config/app_config.dart';
 import 'package:skipq/providers/clinic/clinic_provider.dart';
 import 'package:skipq/providers/user_provider.dart';
@@ -16,35 +17,12 @@ final getIt = GetIt.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  getIt.registerLazySingleton(() => ApiService());
-  getIt.registerLazySingleton(() => FirebaseAuthService());
-  getIt.registerLazySingleton(() => FcmService());
-  getIt.registerSingleton(AppConfig());
-  await getIt.get<AppConfig>().loadAppConfig();
-  await getIt.get<ApiService>().addCookieInceptor();
-
-  AwesomeNotifications().initialize(
-      // set the icon to null if you want to use the default app icon
-      null,
-      [
-        NotificationChannel(
-            channelGroupKey: 'basic_channel_group',
-            channelKey: 'basic_channel',
-            channelName: 'Basic notifications',
-            channelDescription: 'Notification channel for basic tests',
-            defaultColor: Color(0xFF9D50DD),
-            ledColor: Colors.white)
-      ],
-      // Channel groups are only visual and are not required
-      channelGroups: [NotificationChannelGroup(channelGroupkey: 'basic_channel_group', channelGroupName: 'Basic group')],
-      debug: true);
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((value) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-    AppConfig _appConfig = getIt.get<AppConfig>();
 
   @override
   Widget build(BuildContext context) {
@@ -54,8 +32,8 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(create: (_) => ClinicProvider()),
         ],
         child: MaterialApp(
-          debugShowCheckedModeBanner: !_appConfig.isProd,
-          title: _appConfig.isProd?'SkipQ':'SkipQ -Dev',
+          debugShowCheckedModeBanner: false,
+          title: 'SkipQ',
           theme: ThemeData(
             fontFamily: 'Lato',
             primarySwatch: Colors.blue,
--- lib/service/initialize_app.dart
+++ lib/service/initialize_app.dart
@@ -1,4 +1,11 @@
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:skipq/config/app_config.dart';
+import 'package:skipq/main.dart';
+import 'package:skipq/service/api_service.dart';
+import 'package:skipq/service/firebase_services/auth_service.dart';
+import 'package:skipq/service/firebase_services/fcm_service.dart';
 import 'package:skipq/service/firebase_services/firebase_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -10,7 +17,29 @@ class InitializeApp {
       print('here');
       FirebaseApp _firebaseApp = await FirebaseService().inittializeFirebase();
       Admob.initialize();
+      // await Firebase.initializeApp();
+      getIt.registerLazySingleton(() => ApiService());
+      getIt.registerLazySingleton(() => FirebaseAuthService());
+      getIt.registerLazySingleton(() => FcmService());
+      getIt.registerLazySingleton(() => AppConfig());
+      await getIt.get<AppConfig>().loadAppConfig();
+      await getIt.get<ApiService>().addCookieInceptor();
 
+      AwesomeNotifications().initialize(
+          // set the icon to null if you want to use the default app icon
+          null,
+          [
+            NotificationChannel(
+                channelGroupKey: 'basic_channel_group',
+                channelKey: 'basic_channel',
+                channelName: 'Basic notifications',
+                channelDescription: 'Notification channel for basic tests',
+                defaultColor: Color(0xFF9D50DD),
+                ledColor: Colors.white)
+          ],
+          // Channel groups are only visual and are not required
+          channelGroups: [NotificationChannelGroup(channelGroupkey: 'basic_channel_group', channelGroupName: 'Basic group')],
+          debug: true);
       return true;
     } catch (e) {
       print(e);
